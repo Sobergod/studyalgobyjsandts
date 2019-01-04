eae0ed6cc559b6f3b98251e5329fb68ef74d3b80
chenxin <sober_e1@qq.com>
2019-01-04 10:49:03 +0800

    addd链表反转和链表反转注释

--- linklist_js/linklist.js
+++ linklist_js/linklist.js
@@ -50,7 +50,7 @@ class LinkList {
       console.log("未找到插入位置");
       return;
     }
-    const newNode = new Node(newElement);
+    const newNode = new Node_js(newElement);
     newNode.next = currentNode.next;
     currentNode.next = newNode;
   }
@@ -83,9 +83,30 @@ class LinkList {
     // }
     console.log(currentNode);
   }
+  // 链表逆序
+  // 开个新链表不停的往新表的表头插数据
+  reverseList() {
+    // 创建一个新的链表
+    let newLink = new Node_js("head");
+    // 逆转的链表表头不参与逆序
+    let currentNode = this.head.next;
+    while (currentNode !== null) {
+      // 把cuurrentNode.next存上
+      const nextTemp = currentNode.next;
+      // 插入新的链表,当前节点的next指向新链表表头的next
+      currentNode.next = newLink.next;
+      // 新链表表头的next指向当前节点
+      newLink.next = currentNode;
+      // 插下一个
+      currentNode = nextTemp;
+    }
+    // 替换原链表
+    this.head = newLink;
+  }
 }
 let LList = new LinkList();
 LList.insertAfter("haha0", "head");
 LList.insertAfter("haha1", "haha0");
 LList.insertAfter("haha2", "haha1");
+LList.reverseList();
 LList.getAllNodes();
